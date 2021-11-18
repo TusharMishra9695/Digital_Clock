90c79ce5d33a4005a510fb9b123744dbe301e867
Tushar Mishra <67139908+TusharMishra9695@users.noreply.github.com>
2021-11-18 14:11:48 +0530

    #comment Digital clock updated at logic level and now in the dropdown all different timezones are visible

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,44 @@
-import { useState } from 'react';
-export default function Home() { 
+import {validMomentTimezones} from '../utils/options';
+import {useState} from 'react';
+export default function Home() {
     let initialTime = new Date().toLocaleTimeString();
     const [currentTime, setCurrentTime] = useState(initialTime)
-
-    const [optionValue, setOptionValue] = useState("CurrentTime")
+    const [optionValue, setOptionValue] = useState("Asia/Kolkata")
     var momentTz = require("moment-timezone");
     function handleTimeZones(e) {
         setOptionValue(e.target.value)
-        switch (e.target.value)
-        {
-            case "Browser": {
-                console.log(" I am browser ")
-                const updateBrowserTime = () => {
-                    initialTime = new Date().toLocaleTimeString();
-                    setCurrentTime(initialTime)
-                }
-                setInterval(updateBrowserTime,1000)
-                break;
-            }
-            case "Europe": {
-                const updateEuropeTime = () => {
-                    console.log("I am called Europe")
-                    var EuropeTime = momentTz.tz(new Date(), 'Europe/London').format('HH:mm:ss');
-                    setCurrentTime(EuropeTime)
-                }
-                setInterval(updateEuropeTime,1000)
-                break;
-            }
-            case "America": {
-                const updateAmericaTime = () => {
-                    console.log("I am called America")
-                    var AmericaTime = momentTz.tz(new Date(), 'America/Toronto').format('HH:mm:ss');
-                    setCurrentTime(AmericaTime)
-                }
-                setInterval(updateAmericaTime,1000)
-                break;
-            }  
-            case "Asia": {
-                const updateAisaTime = () => {
-                    console.log("I am called Asia")
-                    var AsiaTime = momentTz.tz(new Date(), 'Asia/Taipei').format('HH:mm:ss');
-                    setCurrentTime(AsiaTime)
-                }
-                setInterval(updateAisaTime,1000)
-                break;
-            }       
+        const updateTime = () => {
+            console.log(e.target.value)
+            var timeZoneTime = momentTz.tz(new Date(), e.target.value).format('HH:mm:ss');
+            setCurrentTime(timeZoneTime)
         }
-    }   
-
-
+        setInterval(updateTime, 1000)
+    }
     return (
         <div className="container">
             <center>
-                <h2 style={{fontSize:"40px",color:"white"}}>DIGITAL CLOCK </h2>
-                <h2 style={{ fontSize: "80px", color: "skyblue" }} > {currentTime}</h2>
-                <select style={{ width: "200px", height: "30px", borderRadius: "20px", paddingLeft: "20px" }} onChange={handleTimeZones}
+                <h2 style={
+                    {fontSize: "40px", color: "white"}
+                }>DIGITAL CLOCK
+                </h2>
+                <h2 style={
+                    {fontSize: "80px", color: "skyblue"}
+                }>
+                    {currentTime}</h2>
+                <select style={
+                        {width: "200px", height: "30px", borderRadius: "20px", paddingLeft: "20px"}
+                    }
+                    onChange={handleTimeZones}
                     value={optionValue}>
-                    <option>Please select</option>
-                    <option value="Browser">Browser TimeZone</option>
-                    <option value="America">America/Toronto</option>
-                    <option value="Asia"> Asia/Taipei</option>
-                    <option value="Europe">Europe/London</option>
-                </select>
-
+                    {
+                    validMomentTimezones.map((options, index) => {
+                        return (
+                            <option key={index}
+                                value={options}>
+                                {options} </option>
+                        )
+                    })
+                } </select>
             </center>
         </div>
     )
